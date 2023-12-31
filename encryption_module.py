21dda02c63ea2fec2baf582df138aedf01f1c5aa
G V NIKITHA <109364397+Nikitha1203@users.noreply.github.com>
2023-12-31 13:06:41 +0530

    Add files via upload

---
+++ encryption_module.py
@@ -0,0 +1,22 @@
+from cryptography.fernet import Fernet
+
+class EncryptionModule:
+    def __init__(self):
+        # Generate a key for encryption
+        self.key = Fernet.generate_key()
+        self.cipher_suite = Fernet(self.key)
+
+    def encrypt_data(self, data):
+        # Encrypt data using the key
+        encrypted_data = self.cipher_suite.encrypt(data.encode())
+        return encrypted_data
+
+    def decrypt_data(self, encrypted_data):
+        # Decrypt data using the key
+        decrypted_data = self.cipher_suite.decrypt(encrypted_data).decode()
+        return decrypted_data
+
+    def save_key_to_file(self, file_path):
+        # Save the key to a file
+        with open(file_path, 'wb') as key_file:
+            key_file.write(self.key)
